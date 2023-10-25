6c94fbd1d5d95bea027d0d2c0a7fae3d54ac581e
EDUcamRU <90272454+EDUcamRU@users.noreply.github.com>
2023-10-25 17:33:30 -0400

    Second Update to Prospector.cs

--- Assets/01-Prospector/__Scripts/Prospector.cs
+++ Assets/01-Prospector/__Scripts/Prospector.cs
@@ -11,11 +11,12 @@ public class Prospector : MonoBehaviour {
 
 	[Header("Set in Inspector")]
 	public TextAsset			deckXML;
-
+	public TextAsset			layoutXML;
 
 	[Header("Set Dynamically")]
 	public Deck					deck;
-
+	public Layout				layout;
+	public List<CardProspector> drawPile;
 	void Awake(){
 		S = this;
 	}
@@ -23,6 +24,18 @@ public class Prospector : MonoBehaviour {
 	void Start() {
 		deck = GetComponent<Deck> ();
 		deck.InitDeck (deckXML.text);
+		deck.Shuffle(ref deck.cards);
+		//a 
+		//Card c;
+		//for (int cNum = 0; cNum<deck.cards.Count; cNum++)
+		//{
+        //    c = deck.cards[cNum];
+        //    c.transform.localPosition = new Vector3 ((cNum % 13) * 3, cNum / 13 * 4, 0);
+       // }
+	   layout = GetComponent<Layout>(); // Get the Layout component
+		layout.ReadLayout(layoutXML.text); // Pass LayoutXML to it
+
+		drawPile = ConvertListCardsToListCardProspectors(deck.cards);
 	}
 
 }
