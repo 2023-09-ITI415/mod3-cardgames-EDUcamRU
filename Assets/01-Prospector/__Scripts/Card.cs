16f9133a5e1ad05461552ed966176683db9cb287
EDUcamRU <90272454+EDUcamRU@users.noreply.github.com>
2023-10-25 22:43:36 -0400

    whole buncha stuff
    I did a lot

--- Assets/01-Prospector/__Scripts/Card.cs
+++ Assets/01-Prospector/__Scripts/Card.cs
@@ -16,6 +16,7 @@ public class Card : MonoBehaviour {
 	public GameObject back;  // back of card;
 	public CardDefinition def;  // from DeckXML.xml		
 
+	public SpriteRenderer[] spriteRenderers;
 
 	public bool faceUp {
 		get {
@@ -27,12 +28,67 @@ public class Card : MonoBehaviour {
 		}
 	}
 
-
+	virtual public void OnMouseUpAsButton()
+	{
+		print(name); //When clicked, this outputs the card name
+	}
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		SetSortOrder(0); //Ensures that the card starts properly depth sorted
 	}
 	
+	// If spriteRenderers are present, this funcion will take care of them
+	public void PopulateSpriteRenderers()
+	{
+		//If spriteRenderers is null or empty
+		if (spriteRenderers == null || spriteRenderers.Length == 0)
+		{
+			   // Get SpriteRenderer Components of this GameObject and its children
+            spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+		}
+	}
+
+	//Sets the sortingLayerName on all SpriteRenderer Components
+	public void SetSortingLayerName(string tSLN)
+	{
+        PopulateSpriteRenderers();
+        foreach (SpriteRenderer tSR in spriteRenderers)
+		{
+            tSR.sortingLayerName = tSLN;
+        }
+    }
+
+	//Sets the sortingOrder of all SpriteRenderer Components
+	public void SetSortOrder(int sOrd)
+	{
+        PopulateSpriteRenderers();
+
+        // Iterate through all the spriteRenderers as tSR
+        foreach (SpriteRenderer tSR in spriteRenderers)
+		{
+            if (tSR.gameObject == this.gameObject)
+			{
+                // If the gameObject is this.gameObject, it's the background
+                tSR.sortingOrder = sOrd; // Set it's order to sOrd
+                continue; // And continue to the next iteration of the loop
+            }
+            // Each of the children of this GameObject are named
+            // switch based on the names
+            switch (tSR.gameObject.name)
+			{
+                case "back":
+                    // Set it to the highest layer to cover the other sprites
+                    tSR.sortingOrder = sOrd + 2;
+                    break;
+                case "face":
+                default:
+                    // Set it to the middle layer to be above the background
+                    tSR.sortingOrder = sOrd + 1;
+                    break;
+            }
+        }
+    }
 	// Update is called once per frame
 	void Update () {
 	
